0f054c264b9e9fc3aa5daeb5b66e67a4a42c0538
Nielsm987 <nielsmoons@coachall.be>
2024-02-20 17:41:08 +0100

    feat added promp creation

--- main.py
+++ main.py
@@ -2,11 +2,32 @@ from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 import requests
 import json
+from datetime import datetime
 
 load_dotenv()
 import os
 
 app = Flask(__name__)
+
+
+def is_winter(date_str):
+    date = datetime.strptime(date_str, "%b %d, %Y")
+    return date.month in [12, 1, 2]
+
+
+def is_summer(date_str):
+    date = datetime.strptime(date_str, "%b %d, %Y")
+    return date.month in [6, 7, 8]
+
+
+def is_autumn(date_str):
+    date = datetime.strptime(date_str, "%b %d, %Y")
+    return date.month in [9, 10, 11]
+
+
+def is_spring(date_str):
+    date = datetime.strptime(date_str, "%b %d, %Y")
+    return date.month in [3, 4, 5]
 
 
 def generate_image(description):
@@ -34,7 +55,19 @@ def index():
         data = {}
         for key, value in form_data.items():
             data[key] = value
-        images = generate_image(data["beschrijving"])["images"]
+
+        # Construct the prompt based on the form inputs
+        prompt = f"Genereer een fotorealistische afbeelding met respect voor lichaamsverhoudingen voor een evenement met als titel '{data['naam-evenement']}'"
+        prompt += (
+            f" het evenement kan als volgt beschreven worden '{data['beschrijving']}'"
+        )
+        prompt += f" op de afbeeldingen moeten personen van de leeftijdscategorie '{data['geschikt-voor']}' te zien zijn met een gemengde etnische afkomst"
+        prompt += f" op de afbeeldingen moet je zien dat het {'winter' if is_winter(data['begindatum']) else 'zomer' if is_summer(data['begindatum']) else 'herfst' if is_autumn(data['begindatum']) else 'lente'} is"
+
+        print(prompt)
+
+        # Generate images based on the constructed prompt
+        images = generate_image(prompt)["images"]
         image_tags = []
         for image in images:
             image_url = image["url"]
